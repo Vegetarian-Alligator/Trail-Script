918de24ac2a61c98daa0eafa7a0d92cf3ba9a682
Vegetarian Alligator <9384952+Vegetarian-Alligator@users.noreply.github.com>
2021-06-06 02:30:40 -0500

    Created a version map

--- refact/data.java
+++ refact/data.java
@@ -1,19 +1,18 @@
 package trails2;
+import java.util.ArrayList;
 
 //This class holds all of the variables created by the user.
 //It is designed so that each variable can be an array (a list) or a single variable, by default.
 
 class data{
 
-    String stringValue;
-    double numericValue;
+    //String stringValue;
+    //double numericValue;
     String myName="";
-    boolean isNumeric;
-    
-    //HashMap<String,String> stringValues = new HashMap<String,String>();
-    //HashMap<String,Double> numericValues = new HashMap<String,Double>();
-    //Seriously considered doing that, but we would have to implement so sort of an ordered hash in order to do so.  
-    
+    //boolean isNumeric;
+    ArrayList<String> stringVal = new ArrayList<String>();
+    ArrayList<double> numericVal = new ArrayList<double>();
+    ArrayList<boolean> isnumeric = new ArrayList<boolean>();
     data (String name, String data){
         this.myName=name;
         changeData(data);
@@ -24,11 +23,23 @@ class data{
     }
     
     public String getStringValue(){
-        if (!(isNumeric)){
-            return stringValue;
+        if (!(isNumeric.get(0)){
+            return stringVal.get(0);
         }
-        return Double.toString(numericValue);
+        return Double.toString(numericVal.get(0));
     }
+
+    public String getStringValue(int index){
+        try {
+            if (!(isNumeric.get(index)){
+                return stringVal.get(index);
+            }
+            return Double.toString(numericVal.get(index));
+        } catch (Exception e){
+            System.out.println("Fatal Error. Array likely out of bounds.");
+            System.exit(1);
+        }
+
     
     public boolean isNumeric() {
         return this.isNumeric;
@@ -38,16 +49,22 @@ class data{
         return myName;
     }
     
+
+    public void changeData(String data, int index) {
+
+    }
+
     public void changeData(String data){
-        
         if (data != null) try {
             double d = Double.parseDouble(data);
-            this.numericValue=d;
+            if(numericVal.length()!=0) this.numericVal.get(0)=d;
+            else this.numericVal.add(d);
             isNumeric=true;
             this.stringValue=null;
         } catch (NumberFormatException nfe) {
             isNumeric=false;
-            this.stringValue=data;
+            if (stringVal.length()!=0) this.stringVal.get(0)=data;
+            else stringVal.add(data);
         }
     }
 }
